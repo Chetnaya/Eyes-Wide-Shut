2e539c0a91f8c8a428252d32867ec8b5b0de0162
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-06 20:24:32 +0530

    Allowing player to look up-down, left-right

--- FPS Shooter/Assets/MouseLook.cs
+++ FPS Shooter/Assets/MouseLook.cs
@@ -6,10 +6,12 @@ public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public Transform playerBody;
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked; 
         
     }
 
@@ -19,6 +21,11 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY ;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation =  Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
