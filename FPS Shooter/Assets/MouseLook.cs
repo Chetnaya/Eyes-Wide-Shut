38c2a61a77923361b6bbbad9d3a45e5bc62ca8bf
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-06 20:38:48 +0530

    Comments in MouseLook script

--- FPS Shooter/Assets/MouseLook.cs
+++ FPS Shooter/Assets/MouseLook.cs
@@ -5,27 +5,36 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
+    
+    //To set a refrence to the player body that we want to move
     public Transform playerBody;
+
     float xRotation = 0f;
 
-    // Start is called before the first frame update
     void Start()
     {
+        //Hides and lock the cursor to the centre of the screen
         Cursor.lockState = CursorLockMode.Locked; 
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Get "Mouse X" inputs from input manager in unity and multiplies it with mouse sensitivity
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        
+        //Get "Mouse X" inputs from input manager in unity and multiplies it with mouse sensitivity
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        //Decreases X-Rotation based on Mouse Y
         xRotation -= mouseY ;
 
+        //Makes sure that the player wont rotate above the given range
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        //Rotates Player body around the X-Axis
         transform.localRotation =  Quaternion.Euler(xRotation, 0f, 0f);
+
+        //Rotates Player body around the Y-Axis
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
