6680cf8c240b5a4c657ce800611f82193158bc6b
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-16 20:40:02 +0530

    Player UI Script

---
+++ Eyes Wide Shut/Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGI promptText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void UpdateText(string promtMessage)
+    {
+        promptText.text = promtMessage;
+
+    }
+}
