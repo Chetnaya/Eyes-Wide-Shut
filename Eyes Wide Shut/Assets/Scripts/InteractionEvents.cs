b279198f67bf7d1d5b49d646b1a390aec0273989
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-17 14:15:38 +0530

    Interaction Events Script

---
+++ Eyes Wide Shut/Assets/Scripts/InteractionEvents.cs
@@ -0,0 +1,10 @@
+using UnityEngine.Events;
+// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionEvents : MonoBehaviour
+{
+    public UnityEvent OnInteract;
+    
+}
