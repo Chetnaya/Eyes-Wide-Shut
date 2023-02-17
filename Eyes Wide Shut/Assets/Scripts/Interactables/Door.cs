61c6f2d9edaa85b19a11d59a3aa4f3b501af311b
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-17 11:11:41 +0530

    Door Animation

--- Eyes Wide Shut/Assets/Scripts/Interactables/Door.cs
+++ Eyes Wide Shut/Assets/Scripts/Interactables/Door.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Door : Interactable
 {
+    [SerializeField]
+    private GameObject door;
+    private bool doorOpen;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,7 @@ public class Door : Interactable
     //Design interaction using code
     protected override void Interact()
     {
-        Debug.Log("Interacted with" + gameObject.name);
+        doorOpen = !doorOpen;
+        door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
     }
 }
