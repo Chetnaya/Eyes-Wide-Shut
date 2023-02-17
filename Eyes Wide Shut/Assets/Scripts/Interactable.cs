5273b253cf0d9d20afa98ff2c66638c10d928085
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-17 14:29:19 +0530

    Updated Interactable.cs

--- Eyes Wide Shut/Assets/Scripts/Interactable.cs
+++ Eyes Wide Shut/Assets/Scripts/Interactable.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 //This script follows the "template method pattern"
 public abstract class Interactable : MonoBehaviour
 {
+    //Add or remove an interaction event component to this game object.
+    public bool useEvents;
+
     //Message displayed to the player while looking at an interactable.
     public string promtMessage;
+
+    public virtual string Onlook()
+    {
+        return promtMessage;
+    }
 
     public void baseInteract()
     {
