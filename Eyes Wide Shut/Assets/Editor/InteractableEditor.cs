7d3393d636112234a4b9e2d4fcaa4fcad8b2ef52
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-21 13:48:14 +0530

    Updated InteractableEditor.cs

--- Eyes Wide Shut/Assets/Editor/InteractableEditor.cs
+++ Eyes Wide Shut/Assets/Editor/InteractableEditor.cs
@@ -7,21 +7,39 @@ public class InteractableEditor : Editor
     public override void OnInspectorGUI()
     {
         Interactable interactable = (Interactable)target;
-        base.OnInspectorGUI();
-        if(interactable.useEvents)
+        if(target.GetType() == typeof(EventOnlyInteractable))
         {
-            if(interactable.GetComponent<InteractionEvents>()==null)
+            interactable.promtMessage = EditorGUILayout.TextField("Prompt Message", interactable.promtMessage);
+            EditorGUILayout.HelpBox("EventOnlyInteractable can ONLY use Unity Events", MessageType.Info);
+
+            if(interactable.GetComponent<InteractionEvents>()== null)
             {
+                interactable.useEvents = true;
                 interactable.gameObject.AddComponent<InteractionEvents>();
             }
         }
+
         else
-        {
-            if(interactable.GetComponent<InteractionEvents>() != null)
+        (
+            base.OnInspectorGUI();
+            if(interactable.useEvents)
             {
-                DestroyImmediate(interactable.GetComponent<InteractionEvents>());
+                if(interactable.GetComponent<InteractionEvents>()==null)
+                {
+                    interactable.gameObject.AddComponent<InteractionEvents>();
+                    
+                }
             }
-        }
+            else
+            {
+                if(interactable.GetComponent<InteractionEvents>() != null)
+                {
+                    DestroyImmediate(interactable.GetComponent<InteractionEvents>());
+                }
+            }
+
+        )
+        
             
         
     }
