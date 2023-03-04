f0a02d68b4f6d45ba3efa1ebeee5e0db1d7d7bc0
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-04 21:00:11 +0530

    PlayerMotor.cs - Footsteps sound

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/Player/PlayerMotor.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Player/PlayerMotor.cs
@@ -9,20 +9,28 @@ public class PlayerMotor : MonoBehaviour
     public float speed = 5f;
     private bool isGrounded;
     public float gravity = -9.8f;
-    public float jumpHeight  = 3f;
-    // Start is called before the first frame update
+    public float jumpHeight = 3f;
+
+    public AudioSource footstepSound;
+    private bool isMoving; // add a variable to track if the player is moving
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        footstepSound = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         isGrounded = controller.isGrounded;
+
+        // Stop the footstep sound if the player is not moving
+        if (!isMoving && footstepSound.isPlaying)
+        {
+            footstepSound.Stop();
+        }
     }
 
-    //Receive the inputs from Input Manager and apply them to our character controller.
     public void ProcessMove(Vector2 input)
     {
         Vector3 moveDirection = Vector3.zero;
@@ -30,17 +38,31 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.z = input.y;
 
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+        // Check if the player is moving and play the footstep sound
+        if (moveDirection.magnitude > 0 && isGrounded && !footstepSound.isPlaying)
+        {
+            footstepSound.Play();
+            isMoving = true; // set isMoving to true if the player is moving
+        }
+        else if (moveDirection.magnitude == 0 && footstepSound.isPlaying)
+        {
+            footstepSound.Stop();
+            isMoving = false; // set isMoving to false if the player stops moving
+        }
+
         playerVelocity.y += gravity * Time.deltaTime;
-        if(isGrounded && playerVelocity.y < 0)
+        if (isGrounded && playerVelocity.y < 0)
             playerVelocity.y = -2f;
         controller.Move(playerVelocity * Time.deltaTime);
-        Debug.Log(playerVelocity.y); 
+        Debug.Log(playerVelocity.y);
     }
+
     public void Jump()
     {
-        if(isGrounded)
+        if (isGrounded)
         {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
 }
