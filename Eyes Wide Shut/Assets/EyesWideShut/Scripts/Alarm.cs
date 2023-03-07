7c5afd63a114c1fa9be18087d1e12ab3b6305b20
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-07 16:21:59 +0530

    Alarm.cs Changes

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/Alarm.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Alarm.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateAlarm : MonoBehaviour
+public class Alarm : MonoBehaviour
 {
+    public Transform player;
+    public AudioSource audioSource;
+    public float fadeTime = 2.0f;
+
+    private float maxDistance;
+
     public float speed = 10.0f; // adjust the speed of rotation
 
-    // Update is called once per frame
+    void Start()
+    {
+        maxDistance = audioSource.maxDistance;
+    }
+
     void Update()
     {
+        //Audio Setting
+        float distance = Vector3.Distance(transform.position, player.position);
+        float volume = Mathf.InverseLerp(maxDistance, 0.0f, distance);
+        audioSource.volume = Mathf.Lerp(audioSource.volume, volume, Time.deltaTime * fadeTime);
+
         // Rotate the cylinder around its center
-        
         transform.Rotate(Vector3.forward *  speed * Time.deltaTime);
     }
 }
