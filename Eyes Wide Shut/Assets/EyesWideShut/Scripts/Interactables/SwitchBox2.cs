5e8e08590a5e2cea6f8198e7733a44a214b6a267
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-07 19:16:23 +0530

    Dialogue box for switch box

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/SwitchBox2.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/SwitchBox2.cs
@@ -9,6 +9,14 @@ public class SwitchBox2 : Interactable
 
     // Variable to keep track of whether the lights are on or off
     private bool isLightOn = false;
+    
+    public GameObject dialogue02;
+    public GameObject dialogue03;
+
+    void Start()
+    {
+        dialogue03.SetActive(false);
+    }
 
     // Override the base Interact() method to toggle the light intensity
     protected override void Interact()
@@ -21,6 +29,8 @@ public class SwitchBox2 : Interactable
                 lightComponent.intensity = 1.59f;
             }
             isLightOn = true;
+            dialogue02.SetActive(false);
+            dialogue03.SetActive(true);
         }
         else
         {
@@ -30,6 +40,8 @@ public class SwitchBox2 : Interactable
                 lightComponent.intensity = 0f;
             }
             isLightOn = false;
+            dialogue02.SetActive(true);
+            dialogue03.SetActive(false);
         }
     }
 }
