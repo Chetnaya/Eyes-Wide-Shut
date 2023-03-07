0e1c728ddf757f97fe8adecbf7152d9b0f54ff9e
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-07 19:06:31 +0530

    SwitchBos2.cs

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/SwitchBox2.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/SwitchBox2.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwicthBox2 : MonoBehaviour
+public class SwitchBox2 : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Array of light components attached to switch 1
+    public Light[] switch2Lights;
 
-    // Update is called once per frame
-    void Update()
+    // Variable to keep track of whether the lights are on or off
+    private bool isLightOn = false;
+
+    // Override the base Interact() method to toggle the light intensity
+    protected override void Interact()
     {
-        
+        if (!isLightOn)
+        {
+            // Turn on the lights for switch 1
+            foreach (Light lightComponent in switch2Lights)
+            {
+                lightComponent.intensity = 1.59f;
+            }
+            isLightOn = true;
+        }
+        else
+        {
+            // Turn off the lights for switch 1
+            foreach (Light lightComponent in switch2Lights)
+            {
+                lightComponent.intensity = 0f;
+            }
+            isLightOn = false;
+        }
     }
 }
