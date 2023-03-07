957a4c0ffadeab18d9db56d3ee9ef9ded14be35f
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-07 18:58:47 +0530

    ceiling lights for main base

--- Eyes Wide Shut/Assets/shelterDoor.cs
+++ Eyes Wide Shut/Assets/shelterDoor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class shelterDoor : Interactable
 {
@@ -8,10 +9,14 @@ public class shelterDoor : Interactable
     private GameObject door;
     private bool doorOpen;
 
+    public GameObject dialogue01;
+    public GameObject dialogue02;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Disable Dialogue 02 object initially
+        dialogue02.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,5 +30,18 @@ public class shelterDoor : Interactable
     {
         doorOpen = !doorOpen;
         door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
+
+        // Disable Dialogue 01 and enable Dialogue 02 when the door is opened
+        if (doorOpen)
+        {
+            dialogue01.SetActive(false);
+            dialogue02.SetActive(true);
+        }
+        // Disable Dialogue 02 and enable Dialogue 01 when the door is closed
+        else
+        {
+            dialogue01.SetActive(true);
+            dialogue02.SetActive(false);
+        }
     }
 }
