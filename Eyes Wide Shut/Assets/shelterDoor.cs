c16823e9de2d5686c06b934459425495bc90eab7
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-07 18:12:28 +0530

    Shelter Door animation + interaction

---
+++ Eyes Wide Shut/Assets/shelterDoor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shelterDoor : Interactable
+{
+    [SerializeField]
+    private GameObject door;
+    private bool doorOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Design interaction using code
+    protected override void Interact()
+    {
+        doorOpen = !doorOpen;
+        door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
+    }
+}
