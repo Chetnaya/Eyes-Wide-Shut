965f431bf9c31dcf5c79d0a95e6ba014f2017b3f
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-13 18:54:51 +0530

    MainMenu.cs - Application.Quit

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/MainMenu.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/MainMenu.cs
@@ -10,6 +10,18 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Scene01");
     }
+    public void YesButton()
+    {
+     // save any game data here
+     #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+     #else
+         Application.Quit();
+     #endif
+     
+     }
 
    
 }
