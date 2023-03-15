288e05d4c3012e037a00c939b643bf449ce8799a
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-15 16:51:25 +0530

    Added Ui for loading

--- Eyes Wide Shut/Assets/LevelLoader.cs
+++ Eyes Wide Shut/Assets/LevelLoader.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject button01;
+    public GameObject Button02;
+    public GameObject Button03;
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
     public void loadLevel (int sceneIndex)
     {
         StartCoroutine(LoadAsynchronously(sceneIndex));
@@ -13,9 +21,17 @@ public class LevelLoader : MonoBehaviour
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
+        loadingScreen.SetActive(true);
+
+        button01.SetActive(false);
+        Button02.SetActive(false);
+        Button03.SetActive(false);
+
         while (!operation.isDone)
         {
-            Debug.Log(operation.progress);
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            
+            slider.value = progress;
 
             yield return null;
         }
