511acad0da0ffb374b9a37b1ab3d71a8834ba5ef
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-15 15:52:11 +0530

    LevelLoader.cs

---
+++ Eyes Wide Shut/Assets/LevelLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void loadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
+    }
+}
