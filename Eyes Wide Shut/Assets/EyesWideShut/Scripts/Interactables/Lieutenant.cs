3b741c13073cb1a7aa81de717f54ec434bbbea28
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-19 17:52:59 +0530

    Updated Lieutenant.cs & EyeGlass.cs

---
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/Lieutenant.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lieutenant : Interactable
+{
+    bool dialogue04Enabled = false;
+    public GameObject dialogue04;
+    public GameObject dialogue03;
+    public GameObject dialogue05;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue04.SetActive(false);
+        dialogue05.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dialogue04.activeSelf && !dialogue05.activeSelf)
+        {
+            dialogue05.SetActive(true); // Enable dialogue05 if dialogue04 is disabled and dialogue05 is not already enabled
+        }
+    }
+    
+    protected override void Interact()
+    {
+        dialogue03.SetActive(false);
+        dialogue04.SetActive(true);
+
+
+        dialogue03.SetActive(false);
+        if (!dialogue04Enabled)
+        {
+            dialogue04.SetActive(true);
+            dialogue04Enabled = true;
+        } 
+        else 
+        {
+            dialogue05.SetActive(true);
+        }
+    }
+}
