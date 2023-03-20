c96d8c89c742310e6e59d4caf0982d913f037259
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-20 15:02:15 +0530

    EyeGlass.cs - Added audio

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/EyeGlass.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/EyeGlass.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class EyeGlass : Interactable
 {
+    [SerializeField]
+    private AudioSource eyeGlassintro;
+
     public GameObject dialogue05;
     public GameObject EyeGlassPanel;
+
+    void Start()
+    {
+        eyeGlassintro = eyeGlassintro.GetComponent<AudioSource>();
+    }
     
     protected override void Interact()
     {
         EyeGlassPanel.SetActive(true);
         dialogue05.SetActive(false);
+
+        eyeGlassintro.Play();
 
     }
 }
