c9a28502d1e85a45df7d2dc37612c689ce187b86
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-20 18:15:25 +0530

    Arrow.cs - Added direction functionality

---
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Arrow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Arrow : MonoBehaviour
+{
+    public Transform arrow;
+    public Transform playerTransform;
+    public Transform Objective;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = playerTransform.InverseTransformPoint(Objective.position);
+        float a = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        a += 180;
+        arrow.transform.localEulerAngles = new Vector3(0, 180, a);
+        
+    }
+}
