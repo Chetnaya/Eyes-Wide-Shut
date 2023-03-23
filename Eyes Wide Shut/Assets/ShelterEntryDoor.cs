dcaf5dca01c8383c3727adce1cbb1d99c7000e9a
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-23 19:08:16 +0530

    ShelterEntryDoor.cs changes

---
+++ Eyes Wide Shut/Assets/ShelterEntryDoor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelterEntryDoor : Interactable
+{
+    public GameObject EntryDoor;
+    private bool isOpen;
+
+    protected override void Interact()
+    {
+        isOpen = !isOpen;
+        EntryDoor.GetComponent<Animator>().SetBool("EntryDoorIsOpen", isOpen);
+    }
+}
