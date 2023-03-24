f8910f8f351f39e127e52a38ebbc217faa509e9a
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-24 09:58:48 +0530

    ShelterEntryDoor.cs - Pointer Behaviour

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/ShelterEntryDoor.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Interactables/ShelterEntryDoor.cs
@@ -6,10 +6,26 @@ public class ShelterEntryDoor : Interactable
 {
     public GameObject EntryDoor;
     private bool isOpen;
+    public GameObject PointerImage;
 
     protected override void Interact()
     {
         isOpen = !isOpen;
         EntryDoor.GetComponent<Animator>().SetBool("S.DoorIsOpen", isOpen);
+
+        if(isOpen)
+        {
+            //Disable Pointer
+            PointerImage.SetActive(false);
+
+            //Door open sound
+        }
+        else
+        {
+            //Enable Pointer
+            PointerImage.SetActive(true);
+
+            //Door close sound
+        }
     }
 }
