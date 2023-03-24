de7721556c7df5dd980152997120ccc26d332c39
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-24 10:10:50 +0530

    Arrow.cs - Updating Array for Objectives

--- Eyes Wide Shut/Assets/EyesWideShut/Scripts/Arrow.cs
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Arrow.cs
@@ -7,12 +7,12 @@ public class Arrow : MonoBehaviour
 {
     public Transform arrow;
     public Transform playerTransform;
-    public Transform Objective;
+    public Transform[] Objectives;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = playerTransform.InverseTransformPoint(Objective.position);
+        Vector3 dir = playerTransform.InverseTransformPoint(Objectives[0].position);
         float a = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
         a += 180;
         arrow.transform.localEulerAngles = new Vector3(0, 180, a);
