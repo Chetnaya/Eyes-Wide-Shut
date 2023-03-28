b5fde4a71eaf2d6c4410ebeb1f91ec26f6d331a1
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-03-28 12:48:57 +0530

    Weaponbox.cs - added instruction panel functionality

---
+++ Eyes Wide Shut/Assets/EyesWideShut/Scripts/Weaponbox.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weaponbox : Interactable
+{
+    //-----------------------------
+    //GAME OBJECTS TO BE DISABLED
+    //-----------------------------
+    public GameObject promtText;
+    public GameObject Arrow;
+    public GameObject dialoguebox8;
+
+    //-----------------------------
+    //GAME OBJECTS TO BE ENABLED
+    //-----------------------------
+    public GameObject instructionPanel;
+    public GameObject dialoguebox9;
+    //Players
+    public GameObject player01;
+    public GameObject player02;
+    //-----------------------------
+
+    protected override void Interact()
+    {
+        //Disabling
+        promtText.SetActive(false);
+        Arrow.SetActive(false);
+        dialoguebox8.SetActive(false);
+
+        //Enabling
+        instructionPanel.SetActive(true);
+
+    }
+
+    //Instruction Panel Button Functionality
+    public void PlayersSwitch()
+    {
+        player01.SetActive(false);
+        player02.SetActive(true);
+
+        dialoguebox9.SetActive(true);
+
+        instructionPanel.SetActive(false);
+    }
+}
