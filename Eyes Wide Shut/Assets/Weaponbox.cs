2bdceba26857391f1958a289943ab561fcb9665e
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-03-28 11:48:47 +0530

    Weaponbox.cs Updates

--- Eyes Wide Shut/Assets/Weaponbox.cs
+++ Eyes Wide Shut/Assets/Weaponbox.cs
@@ -6,10 +6,16 @@ public class Weaponbox : Interactable
 {
     public GameObject player1;
     public GameObject player2;
+
+    public GameObject promtText;
+    public GameObject Arrow;
     
     protected override void Interact()
     {
         player1.SetActive(false);
         player2.SetActive(true);
+
+        promtText.SetActive(false);
+        Arrow.SetActive(false);
     }
 }
